ba8f09a2cfe144fafa244b0c4f265f03cea23f85
Naren-HappySanz <naren@happysanztech.com>
2019-11-22 09:53:27 +0530

    Pull

--- app/src/main/java/com/skilex/skilexserviceperson/activity/fragmentmodule/ongoing/InitiatedServiceActivity.java
+++ app/src/main/java/com/skilex/skilexserviceperson/activity/fragmentmodule/ongoing/InitiatedServiceActivity.java
@@ -1,6 +1,8 @@
 package com.skilex.skilexserviceperson.activity.fragmentmodule.ongoing;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
@@ -10,11 +12,16 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.gson.Gson;
 import com.skilex.skilexserviceperson.R;
 import com.skilex.skilexserviceperson.bean.support.OngoingService;
@@ -34,7 +41,7 @@ import org.json.JSONObject;
 
 import static android.util.Log.d;
 
-public class InitiatedServiceActivity extends BaseActivity implements IServiceListener, DialogClickListener, View.OnClickListener {
+public class InitiatedServiceActivity extends BaseActivity implements OnMapReadyCallback, IServiceListener, DialogClickListener, View.OnClickListener {
 
     private static final String TAG = InitiatedServiceActivity.class.getName();
     private ServiceHelper serviceHelper;
@@ -72,6 +79,17 @@ public class InitiatedServiceActivity extends BaseActivity implements IServiceLi
         btnNext = findViewById(R.id.btn_next);
         btnNext.setOnClickListener(this);
 
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        }
+
+//        livLoc = getIntent().getParcelableExtra("dist");
+
+
+        mapView = findViewById(R.id.mapview);
+        mapView.onCreate(mapViewBundle);
+        mapView.getMapAsync(this);
         loadServiceDetails();
     }
 
@@ -143,6 +161,16 @@ public class InitiatedServiceActivity extends BaseActivity implements IServiceLi
                     cusName.setText(getServiceData.getString("contact_person_name"));
                     cusPhone.setText(getServiceData.getString("contact_person_number"));
                     cusAddress.setText(getServiceData.getString("service_address"));
+
+                    String string = getServiceData.getString("service_latlon");
+                    String[] parts = string.split(",");
+                    String part1 = parts[0]; // 004
+                    String part2 = parts[1]; // 034556
+                    Double lat = Double.valueOf(part1);
+                    Double longi = Double.valueOf(part2);
+                    LatLng latLng = new LatLng(lat, longi);
+                    showMarker(latLng);
+
                 } else if (res.equalsIgnoreCase("initiateService")) {
                     finish();
 
@@ -153,6 +181,23 @@ public class InitiatedServiceActivity extends BaseActivity implements IServiceLi
             }
         }
 
+    }
+
+    private void showMarker(@NonNull LatLng latLng) {
+//        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+//        int height = 60;
+//        int width = 30;
+//        BitmapDrawable bitmapdraw = (BitmapDrawable) getResources().getDrawable(R.drawable.commando);
+//        Bitmap b = bitmapdraw.getBitmap();
+//        final Bitmap smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
+        if (currentLocationMarker == null) {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+            currentLocationMarker = mMap.addMarker(new MarkerOptions().position(latLng));
+            mapView.onResume();
+        }
+//        else {
+//            MarkerAnimation.animateMarkerToGB(currentLocationMarker, latLng, new LatLngInterpolator.Spherical());
+//        }
     }
 
     @Override
@@ -176,5 +221,62 @@ public class InitiatedServiceActivity extends BaseActivity implements IServiceLi
         } else {
             AlertDialogHelper.showSimpleAlertDialog(this, "No Network connection available");
         }
+    }
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAP_VIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mapView.onSaveInstanceState(mapViewBundle);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+//        mMap.setMinZoomPreference(12);
+        LatLng ny = new LatLng(40.7143528, -74.0059731);
     }
 }
